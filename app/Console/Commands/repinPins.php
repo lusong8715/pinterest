6aa219523792b2842ce787fdc5015ab090938884
lusong <lusong@jeulia.net>
2017-12-07 10:46:29 +0800

    repin条件修改

--- app/Console/Commands/repinPins.php
+++ app/Console/Commands/repinPins.php
@@ -49,24 +49,26 @@ class repinPins extends Command
         $username = $config->username;
         $accessToken = $config->access_token;
         $updateSitemap = false;
-        // 原生pin过去30天内saves增长大于3时,repin
-        $date = date('Y-m-d', strtotime('-30 day'));
+        $date7 = date('Y-m-d', strtotime('-7 day'));
+        $date30 = date('Y-m-d', strtotime('-30 day'));
+
+        // 原生pin过去30天内saves增长大于3并且发布时间超过一周时,repin
         $rows = DB::select("select pins.pin_id as pinid, custom.title, custom.image, custom.board, custom.note, custom.link 
                     from custom inner join pins on pins.pin_id = custom.pin_id 
                     where custom.status = '1'
                     and pins.advertised = '0'
+                    and pins.created_at < ?
                     and not exists(select 1 from pins as p where p.root_pin = custom.pin_id) 
-                    and (select sum(saves_change) as sumsave from pin_data_history as pdh where pdh.pins_id = pins.id and pdh.update_date > ? having sumsave > ?)", [$date, 3]);
+                    and (select sum(saves_change) as sumsave from pin_data_history as pdh where pdh.pins_id = pins.id and pdh.update_date > ? having sumsave > ?)", [$date7, $date30, 3]);
 
-        // repin过的pin过去7天内saves增长大于10并且repin时间超过一周时,再次repin
-        $date = date('Y-m-d', strtotime('-7 day'));
+        // repin过的pin过去7天内saves增长大于10并且发布时间超过一周时,再次repin
         $rows2 = DB::select("select p.root_pin as pinid, p.title, p.board, c.image, c.note, c.link, max(p.created_at) as rdate 
                       from pins as p inner join custom c on p.root_pin = c.pin_id 
                       where p.way = '1' 
                       and p.advertised = '0' 
                       and (select sum(saves_change) as sumsave from pin_data_history as pdh where pdh.pins_id = p.id and pdh.update_date > ? having sumsave > ?) 
                       group by p.root_pin 
-                      having rdate < ?", [$date, 10, $date]);
+                      having rdate < ?", [$date7, 10, $date7]);
 
         $result = $rows + $rows2;
         foreach ($result as $row) {
