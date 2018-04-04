3dc402f1168e9b8ea21362e130b22adc0b20a500
lusong <lusong@jeulia.net>
2018-04-04 10:54:29 +0800

    top pin统计周期改为15天

--- app/Http/Controllers/TopPinsController.php
+++ app/Http/Controllers/TopPinsController.php
@@ -7,12 +7,14 @@ use Illuminate\Support\Facades\DB;
 class TopPinsController extends Controller
 {
     public function index() {
+        $date = date('Y-m-d', strtotime('-15 day'));
         $result = DB::select("select p.saves, p.comments, p.image_url, if((char_length(p.original_link)>10), p.original_link, p.url) as link, sum(pdh.saves_change) as saves_change 
                     from pins as p inner join pin_data_history as pdh 
                     on p.id = pdh.pins_id 
+                    where pdh.update_date > ?
                     group by p.id 
                     order by saves_change desc, p.id desc
-                    limit 100");
+                    limit 100", [$date]);
         echo json_encode($result);
         return;
     }
